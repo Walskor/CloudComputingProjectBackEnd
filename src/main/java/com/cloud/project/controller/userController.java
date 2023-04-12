69f837e146be908d6e922d03d3f5e38c278ccd76
walskor <1148905353@qq.com>
2023-04-12 19:17:41 +0800

    User Module

--- src/main/java/com/cloud/project/controller/userController.java
+++ src/main/java/com/cloud/project/controller/userController.java
@@ -1,12 +1,23 @@
 package com.cloud.project.controller;
 
+import com.cloud.project.common.ResponseEntity;
+import com.cloud.project.entity.User;
+import com.cloud.project.service.impl.UserService;
 import jakarta.persistence.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class userController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Integer id) {return null;}
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {return null;}
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {return null;}
 }
